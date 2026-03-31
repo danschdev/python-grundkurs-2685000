df07d57bfbe70ca9ff1db26595a0506bbeee5b99
danschdev <daniel_schnelle@web.de>
2026-03-31 06:56:43 +0000

    Implement simple calculator

--- script.py
+++ script.py
@@ -18,3 +18,22 @@
 # 4. Das Programm führt die Berechnung durch und gibt das Ergebnis aus.
 
 # Optional: Erweitern Sie den Taschenrechner um weitere Funktionen wie Potenzierung oder Modulo.
+
+operator = input("Geben Sie den Operator ein")
+operand1 = float(input("Geben Sie die erste Zahl ein"))
+operand2 = float(input("Geben Sie die zweite Zahl ein"))
+
+if (operator == "+"):
+  print(operand1 + operand2)
+elif (operator == "-"):
+  print(operand1 - operand2)
+elif (operator == "*"):
+  print(operand1 * operand2)
+elif (operator == "/"):
+  print(operand1 / operand2)
+elif (operator == "^" or operator == "**"):
+  print(operand1 ** operand2)
+elif (operator == "%"):
+  print(operand1 % operand2)
+else:
+  print("Ungültige Eingabe")
